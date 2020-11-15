e692069ba4de9a4595c83c9cecbbac58879e274f
faffeldt <felix.affeldt9698@gmail.com>
2020-11-15 13:47:32 +0100

    Adds some basic elemnts to vue app

--- vue-learning/src/App.vue
+++ vue-learning/src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="app">
     <h1>{{ heading }}</h1>
+    
+    <div>
+      <h2>{{ heading_basic_list }}</h2>
+      <ol>
+        <li v-for="item in items">
+          {{ item.name }}: {{ item.detail }}
+        </li>
+      </ol>
+    </div>
+
+    <div>
+      <h2>{{ heading_handling_user_input }}</h2>
+      <p>{{ handling_user_input_message }}</p>
+      <button v-on:click="reverseMessage">Reverse message</button>
+    </div>
+
+    <div>
+      <h2>{{ heading_handling_input_model }}</h2>
+      <p>{{ handling_input_model_value }}</p>
+      <input v-model="handling_input_model_value">
+    </div>
   </div>
 </template>
 
@@ -9,7 +30,21 @@ export default {
   name: 'app',
   data () {
     return {
-      heading: 'Welcome to my learning Vue App'
+      heading: 'Welcome to my learning Vue App',
+      heading_basic_list: 'How to render a basic listing:',
+      items: [
+        { name: 'Brot', detail: 'Schwarzbrot, Graubrot, etc.' },
+        { name: 'Nudeln', detail: 'Spaghetti, Spirelli, Lasagne, etc.' }
+      ],
+      heading_handling_user_input: 'How to handle user input and basic custom methods:',
+      handling_user_input_message: 'Lorem ipsum dolor amen sit',
+      heading_handling_input_model: 'How to handle input fields and model:',
+      handling_input_model_value: 'Lorem ipsum dolor amen sit'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.handling_user_input_message = this.handling_user_input_message.split('').reverse().join('')
     }
   }
 }
